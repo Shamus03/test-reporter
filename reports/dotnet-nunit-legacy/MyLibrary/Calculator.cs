96ad96d584d5d800e000b3bc1d9e981960fef3a5
Shamus Taylor <jtaylor@labattfood.com>
2025-03-05 09:46:34 -0600

    Add nunit-legacy fixture generation code

---
+++ reports/dotnet-nunit-legacy/MyLibrary/Calculator.cs
@@ -0,0 +1,9 @@
+﻿namespace MyLibrary
+{ 
+    public class Calculator
+    {
+        public int Sum(int a, int b) => a + b;
+
+        public int Div(int a, int b) => a / b;
+    }
+}
---
+++ reports/dotnet-nunit-legacy/NUnitLegacyTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NUnitLegacyTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NUnitLegacyTests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("28c713f2-6e53-41a0-9b2c-6ac6ff1db80b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
